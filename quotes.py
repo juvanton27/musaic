4ae8b2cbc7d59f32ed427648bbf8758dca77c611
Julien Van Tongerloo <julien.vantongerloo@hotmail.com>
2023-08-09 10:29:21 +0200

    add - music

--- quotes.py
+++ quotes.py
@@ -4,10 +4,11 @@ from datetime import datetime
 import csv
 import random
 import textwrap
+from music import create_model, generate_long_audio
 
 account_name = 'quotes.musaic'
 input_path = os.path.join(os.path.dirname(__file__), 'input')
-output_path = os.path.join(os.path.dirname(__file__), 'output')
+output_path = os.path.join(os.path.dirname(__file__), 'video_output')
 
 def cleaned_sentence(sentence: str, is_filename: bool = False) -> str:
   max_line_length = 20 
@@ -16,11 +17,8 @@ def cleaned_sentence(sentence: str, is_filename: bool = False) -> str:
     lines = textwrap.wrap(sentence, width=max_line_length)
     sentence = '\n'.join(lines)
   return sentence
-  
 
-def generate_video(theme: str, first_part: str, second_part: str, video_path: str):
-  duration = 11.5
-
+def generate_video(theme: str, first_part: str, second_part: str, video_path: str, audio_path: str, duration: int):
   # Checks duration of video 
   command = f'ffprobe -i {video_path} -show_entries format=duration -v quiet -of csv="p=0"'
   result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
@@ -31,7 +29,7 @@ def generate_video(theme: str, first_part: str, second_part: str, video_path: st
 
   # Generates video
   output_file = os.path.join(output_path, f'{cleaned_sentence(first_part, True)}.mp4')
-  subprocess.run(f'ffmpeg -i {video_path} -t {duration} -vf "\
+  subprocess.run(f'ffmpeg -i {video_path} -i {audio_path} -t {duration} -vf "\
     drawtext=text=\'{cleaned_sentence(theme.upper())}\':x=(w-tw)/2:y=(h-th)/5:fontsize=w/20:fontcolor=white:fontfile={os.path.join(input_path, "fonts/Poppins/Poppins-Regular.ttf")},\
     drawtext=text=\'{cleaned_sentence(first_part)}\':x=(w-tw)/2:y=(h-th)/2:fontsize=w/10:fontcolor=white:fontfile={os.path.join(input_path, "fonts/KudryashevDisplay/fontspring-demo-kdp45.otf")}:enable=\'between(t,0,9)\':box=1:boxcolor=black:boxborderw=10:line_spacing=10,\
     drawtext=text=\'{cleaned_sentence(second_part)}\':x=(w-tw)/2:y=(h-th)/2:fontsize=w/10:fontcolor=white:fontfile={os.path.join(input_path, "fonts/KudryashevDisplay/fontspring-demo-kdp45.otf")}:enable=\'between(t,10,{duration})\':box=1:boxcolor=black:boxborderw=10:line_spacing=10,\
@@ -45,16 +43,23 @@ if not os.path.exists(output_path):
   os.mkdir(output_path)
 
 with open('data.csv', 'r') as csv_file:
+  duration = 11.5
   # Reads inputs
   video_folder = os.path.join(input_path, 'videos')
   file_list = list(filter(lambda filename: filename.lower().endswith('.mp4'), os.listdir(video_folder)))
   # Reads CSV
   csv_reader = csv.reader(csv_file, delimiter=';')
   next(csv_reader)
+  # Create song model
+  model = create_model()
+  sentence = 'chill love piano song'
   for row in csv_reader:
     theme, first_part, second_part = row
+    print(f'Generating {first_part}')
+    audio_path = generate_long_audio(model, sentence, duration, topk=250, topp=0, temperature=1.0, cfg_coef=3.0, overlap=10)
     video_path = os.path.join(video_folder, random.choice(file_list))
     try:
-      generate_video(theme, first_part, second_part, video_path)
+      generate_video(theme, first_part, second_part, video_path, audio_path, duration)
     except Exception as e:
       print(e)
+    break
