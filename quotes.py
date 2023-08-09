2a9cbf5beb98645229fabdedab4ecccb784a58d0
Julien Van Tongerloo <julien.vantongerloo@hotmail.com>
2023-08-09 09:58:05 +0200

    fix - filename output + account name

--- quotes.py
+++ quotes.py
@@ -5,23 +5,21 @@ import csv
 import random
 import textwrap
 
+account_name = 'quotes.musaic'
 input_path = os.path.join(os.path.dirname(__file__), 'input')
 output_path = os.path.join(os.path.dirname(__file__), 'output')
 
-def cleaned_sentence(sentence: str) -> str:
+def cleaned_sentence(sentence: str, is_filename: bool = False) -> str:
+  max_line_length = 20 
   sentence = sentence.replace('\'', " ")
-  max_line_length = 20  # Adjust as needed
-
-  # Split the text into lines of max_line_length characters or less
-  lines = textwrap.wrap(sentence, width=max_line_length)
-
-  # Join the lines with '\n' to create a multi-line text
-  return '\n'.join(lines)
+  if not is_filename: 
+    lines = textwrap.wrap(sentence, width=max_line_length)
+    sentence = '\n'.join(lines)
+  return sentence
   
 
 def generate_video(theme: str, first_part: str, second_part: str, video_path: str):
   duration = 11.5
-  timestamp = datetime.now().timestamp()
 
   # Checks duration of video 
   command = f'ffprobe -i {video_path} -show_entries format=duration -v quiet -of csv="p=0"'
@@ -32,12 +30,12 @@ def generate_video(theme: str, first_part: str, second_part: str, video_path: st
     return
 
   # Generates video
-  output_file = os.path.join(output_path, f'{timestamp}.mp4')
+  output_file = os.path.join(output_path, f'{cleaned_sentence(first_part, True)}.mp4')
   subprocess.run(f'ffmpeg -i {video_path} -t {duration} -vf "\
     drawtext=text=\'{cleaned_sentence(theme.upper())}\':x=(w-tw)/2:y=(h-th)/5:fontsize=w/20:fontcolor=white:fontfile={os.path.join(input_path, "fonts/Poppins/Poppins-Regular.ttf")},\
     drawtext=text=\'{cleaned_sentence(first_part)}\':x=(w-tw)/2:y=(h-th)/2:fontsize=w/10:fontcolor=white:fontfile={os.path.join(input_path, "fonts/KudryashevDisplay/fontspring-demo-kdp45.otf")}:enable=\'between(t,0,9)\':box=1:boxcolor=black:boxborderw=10:line_spacing=10,\
     drawtext=text=\'{cleaned_sentence(second_part)}\':x=(w-tw)/2:y=(h-th)/2:fontsize=w/10:fontcolor=white:fontfile={os.path.join(input_path, "fonts/KudryashevDisplay/fontspring-demo-kdp45.otf")}:enable=\'between(t,10,{duration})\':box=1:boxcolor=black:boxborderw=10:line_spacing=10,\
-    drawtext=text=\'@loveisntreal\':x=(w-tw)/2:y=4*(h-th)/5:fontsize=w/25:fontcolor=white:fontfile={os.path.join(input_path, "fonts/Poppins/Poppins-Regular.ttf")}" -c:v libx264 -c:a copy {output_file}', 
+    drawtext=text=\'@{account_name}\':x=(w-tw)/2:y=4*(h-th)/5:fontsize=w/25:fontcolor=white:fontfile={os.path.join(input_path, "fonts/Poppins/Poppins-Regular.ttf")}" -c:v libx264 -c:a copy \'{output_file}\'', 
     shell= True, stderr=subprocess.DEVNULL
   )
   if not os.path.exists(output_file) or (os.path.exists(output_file) and os.path.getsize(output_file) == 0):
