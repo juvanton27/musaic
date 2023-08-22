94f0d4c0dd8538f3581eddedeac31d9f01e7d002
Julien Van Tongerloo <julien.vantongerloo@hotmail.com>
2023-08-22 10:29:24 +0200

    fix

--- login.py
+++ login.py
@@ -2,16 +2,15 @@ import platform
 import os
 import time
 
+from dotenv import load_dotenv
 from selenium.webdriver import Chrome, ChromeOptions
 
+load_dotenv()
 options = ChromeOptions()
 options.add_argument("--window-size=1920,1080")
 options.add_argument("--enable-logging")
 options.add_argument("--v=1")
 options.add_argument(f"user-data-dir={os.getenv('USER_DATA_DIR')}")
-if platform.system() == 'Darwin':
-  options.binary_location = os.getenv('BINARY_LOCATION')
-
 chromedriver_path = os.getenv('CHROMEDRIVER_PATH')
 os.environ["PATH"] += os.pathsep + chromedriver_path
 
